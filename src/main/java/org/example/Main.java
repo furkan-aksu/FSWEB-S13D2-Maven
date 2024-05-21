008ea306305115cd17cf4935270c733feb9cbd9b
furkan-aksu <161708506+furkan-aksu@users.noreply.github.com>
2024-05-21 19:28:42 +0300

    secondJava

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,120 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+
+    public static boolean isPalindrome(int number) {
+
+        if (number < 0) {
+            number = -number;
+        }
+
+        int reversed = 0;
+        int original = number;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int reversed = reverse(number);
+        int originalDigits = getDigitCount(number);
+        int reversedDigits = getDigitCount(reversed);
+
+        while (reversed > 0) {
+            int digit = reversed % 10;
+            switch (digit) {
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
+            }
+            reversed /= 10;
+        }
+        for (int i = 0; i < originalDigits - reversedDigits; i++) {
+            System.out.print("Zero ");
+        }
+
+        System.out.println();
+    }
+
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+
+
+    public static int getDigitCount(int number) {
+        if (number == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
     }
 }
+
+
+
